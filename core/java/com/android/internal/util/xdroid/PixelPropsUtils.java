87509e139f29b3342ddc2627029faf3b613f3feb
Danny Lin <danny@kdrag0n.dev>
2021-12-10 17:27:19 +0000

    Alter model name to avoid SafetyNet HW attestation enforcement
    As of September 2, Google is enforcing SafetyNet's previously
    opportunistic hardware-backed attestation based on device information.
    Append a space to the device model name in order to avoid such
    enforcement.
    
    Change-Id: Ib7779e0aae40cab3730a56785e9231896917ab0a
    (cherry picked from commit 4a58bbb110707a93d9620e5643be0ee7d3c7c86a)

--- core/java/com/android/internal/util/xdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/xdroid/PixelPropsUtils.java
@@ -102,7 +102,7 @@ public class PixelPropsUtils {
         propsToChange.put("MANUFACTURER", "Google");
         propsToChange.put("DEVICE", "redfin");
         propsToChange.put("PRODUCT", "redfin");
-        propsToChange.put("MODEL", "Pixel 5");
+        propsToChange.put("MODEL", "Pixel 5" + " ");
         propsToChange.put("FINGERPRINT", "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys");
         propsToChangePixel3XL = new HashMap<>();
         propsToChangePixel3XL.put("BRAND", "google");
